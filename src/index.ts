e73f733fa8e6308e337d4e680e9eebfd6525d083
son.tran <son.tran@team.enouvo.com>
2023-10-17 21:16:47 +0700

    feat(master): add more origin to support cors

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const bootstrap = async () => {
     app.use(
       '*',
       cors({
-        origin: ['http://localhost:5173'],
+        origin: ['http://localhost:5173', 'https://skillfy.vercel.app'],
         credentials: true,
       })
     );
